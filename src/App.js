cd2f7bdd705e7566db27228911363375ac6a0f2d
julzel <julio@zarmada.com>
2021-01-28 16:14:44 -0600

    page in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 // local imports
-import "./fonts.scss";
-import './App.scss';
-import dnatureLogo from "./assets/images/dnature-logo.png";
-import Dna from './components/Dna';
+import "./global-styles/reset.scss";
+import "./global-styles/fonts.scss";
+import "./global-styles/global.scss";
+import Home from './pages/Home/Home';
+import Layout from './components/Layout/Layout';
 
 const App = () => {
 
-  useEffect(() => {
+  const appInit = () => {
     let vh = window.innerHeight * 0.01;
     window.document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
+  }
+
+  useEffect(() => {
+    appInit();
+    window.addEventListener('resize', appInit);
+    return () => window.removeEventListener('resize', appInit);
+  }, []);
 
   return (
-    <div className="page-under-construction">
-      <div className="logo-wrapper">
-        <img src={dnatureLogo} alt="DNAture logo" />
-        <div className="temp" />
-      </div>
-      <Dna />
-    </div>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/" render={() => <Home />} />
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
  
